bc3e0c20ffdf4dfc349ec90e8637bf75c0438472
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-14 01:06:13 +0530

    tests2

--- server/server.js
+++ server/server.js
@@ -8,12 +8,9 @@ dotenv.config({ path: "./config.env" });
 const cookieParser = require('cookie-parser');
 const router = require('./routes/approutes');
 connectdatabase();
+
+app.use(cors())
 app.use(express.json())
-app.use(cors({
-      origin:[process.env.FRONTEND_URL],
-      methods:["GET","POST","PUT","DELETE"],
-      credentials:true
-}))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use("/api/v1",router)
