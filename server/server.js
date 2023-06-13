5431dbd0838ebbd422b35f4c7b037e026428fa6c
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-13 22:48:38 +0530

    cors update

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const cookieParser = require('cookie-parser');
 const router = require('./routes/approutes');
 connectdatabase();
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+      origin:[process.env.FRONTEND_URL],
+      methods:["GET","POST","PUT","DELETE"],
+      credentials:true
+}))
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use("/api/v1",router)
