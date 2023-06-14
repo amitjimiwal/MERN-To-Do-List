f9de62eb30bdfe0b2de93fe9e6ff53ff82c80dd6
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-14 10:36:34 +0530

    cookie config updates

--- server/middlewares/sendCookie.js
+++ server/middlewares/sendCookie.js
@@ -4,7 +4,7 @@ const sendCookie=(user,res,message,statuscode=200)=>{
       res.status(statuscode).cookie("token",token,{
             httpOnly:true,
             maxAge:15*60*1000,
-            sameSite: process.env.NODE_ENV==="DEV"? "lax":"none",
+            sameSite: process.env.NODE_ENV==="DEV"? "lax":"None",
             secure: process.env.NODE_ENV==="DEV"? false:true
       }).json({
             success:'true',
