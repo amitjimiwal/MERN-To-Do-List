6fb695473c0ab6c9bff0b76537b51a54eee58b61
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-06-18 20:51:12 +0530

    removed console

--- server/middlewares/sendCookie.js
+++ server/middlewares/sendCookie.js
@@ -1,6 +1,5 @@
 const jwt=require('jsonwebtoken')
 const sendCookie=(user,res,message,statuscode=200)=>{
-      console.log(process.env.NODE_ENV)
       const token=jwt.sign({_id:user._id},process.env.SECRET_KEY)
       res.status(statuscode).cookie("token",token,{
             httpOnly:true,
