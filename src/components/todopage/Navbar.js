4fa67ce65dc79a35bacb6c5627fb00a89075eee3
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-07-14 12:12:53 +0530

    lopgout btn

--- src/components/todopage/Navbar.js
+++ src/components/todopage/Navbar.js
@@ -1,11 +1,35 @@
-import React from "react";
-
+import React ,{useRef} from "react";
+import LoadingBar from "react-top-loading-bar";
+import { useDispatch } from "react-redux";
+import auth from "../../api/auth";
+import { toast } from "react-hot-toast";
+import { setUser } from "../../store/todoSlice";
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const ref = useRef(null);
+  const logout = async () => {
+    ref.current.continuousStart();
+    ref.current.complete();
+    try {
+      const output = await auth("logout", {});
+      if (output.success) {
+        toast.success(output.message);
+        dispatch(
+          setUser({
+            isloggedin: false,
+          })
+        );
+      } else {
+        toast.error(output.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
   return (
     <nav className="navbar navbar-expand navbar-light navbar-custom d-flex align-items-center">
-      <h1 className="navbar-brand">
-        ToD0Web
-      </h1>
+      <LoadingBar color="red" ref={ref} />
+      <h1 className="navbar-brand">ToD0Web</h1>
       <button
         className="navbar-toggler"
         type="button"
@@ -19,15 +43,13 @@ const Navbar = () => {
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="">
+          {/* <li className="nav-item">
+            < className="nav-link" href="">
               Profile
-            </a>
-          </li>
+            </>
+          </li> */}
           <li className="nav-item">
-            <a className="nav-link" href="">
-              Logout
-            </a>
+            <button type="button" className="btn btn-info" onClick={logout}>Logout</button>
           </li>
         </ul>
       </div>
