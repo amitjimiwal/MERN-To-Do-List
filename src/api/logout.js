10075a38267d125f22a8b0ebe1aaa248eebe8672
Amit <90555965+amitjimiwal@users.noreply.github.com>
2023-07-14 12:13:18 +0530

    feat : logout

---
+++ src/api/logout.js
@@ -0,0 +1,22 @@
+const logout=async(method,data={})=>{
+      //function for login and register and logout
+  try {
+        const response = await fetch(
+          `${process.env.REACT_APP_BACKEND_BASE_URL}/${method.toLowerCase()}`,
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(data),
+            credentials:"include"
+          }
+        );
+        // getting the resposne
+       const jsonresponse= await response.json()
+       return jsonresponse;
+  
+      } catch (error) {
+        console.log(error)
+      }
+  }
